1b51d49f675ee40cdaf17204802ccb15d9ae725b
Alex-dotcom844 <alexvincent306@gmail.com>
2025-03-25 13:20:16 +0100

    first commit

---
+++ project-1.js
@@ -0,0 +1,14 @@
+// Variables
+
+let btn = document.querySelector('#new-quote');
+let quote = document.querySelector('.quote');
+let person = document.querySelector('.person');
+
+const quotes = [{
+    quote: '"The best way to find yourself in theservice of others."',
+    person: ' Mahatma Gandhi'
+}, {
+    quote: '"If you want to live a happy life, tie it to a goal, not to people or things."',
+    person: ' Albert Einstein'
+}]
+
